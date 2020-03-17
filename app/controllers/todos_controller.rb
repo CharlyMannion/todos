d1be0ee2bdc3b025fddefa7e4b0669beda3850db
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-17 11:31:23 +0000

    TDD's the creation of new todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,25 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    @todo.save
+    redirect_to todos_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:title)
   end
 end
---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature "User creates todo" do
+  scenario "successfully" do
+    visit "/"
+
+    click_on "Add a new todo"
+    fill_in "Title", with: "My First Todo"
+    click_on "Submit"
+
+    expect(page).to have_css '.todos li', text: "My First Todo"
+    # expect(page).to have_content "My First Todo"
+  end
+end
