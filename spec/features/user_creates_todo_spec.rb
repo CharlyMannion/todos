7790a033bdf69a79bd711c53b3984cbadc6bd8e8
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-18 15:41:36 +0000

    extracts a method for matching css

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -4,6 +4,7 @@ feature "User creates todo" do
   scenario "successfully" do
     sign_in
     create_todo("My First Todo")
-    expect(page).to have_css '.todos li', text: "My First Todo"
+    expect(page).to display_todo "My First Todo"
+    # expect(page).to have_css '.todos li', text: "My First Todo"
   end
 end
