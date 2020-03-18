079ff9bad42ad653bc30f1eda6e2ff35b86e02cc
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-18 15:32:54 +0000

    refactors specs, extracting a todo feature helper

--- spec/features/user_completes_todo_spec.rb
+++ spec/features/user_completes_todo_spec.rb
@@ -3,11 +3,7 @@ require 'rails_helper'
 feature "User completes a todo" do
   scenario "successfully" do
     sign_in
-
-    click_on "Add a new todo"
-    fill_in "Title", with: "A Todo to mark complete"
-    click_on "Submit"
-
+    create_todo("A Todo to mark complete")
     click_on "Mark complete"
     expect(page).to have_css '.todos li.completed', text: "A Todo to mark complete"
   end
